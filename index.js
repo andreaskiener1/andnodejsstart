732ca1ba0809d0de0011ed9ba843e7a740360581
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 16:48:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express')
-//var path = require('path')
+var path = require('path')
 
 var app = express()
 app.set('port', (process.env.PORT || 5000))
 
-//var dateipfad = path.join(__dirname, 'public', 'caesar26.html')
+var dateipfad = path.join(__dirname, 'public', 'caesar26.html')
  
 app.get('/', function(request, response) {
    //response.sendFile(dateipfad)
-   response.send('<body> <h2>Berechne das Menschenalter</h2> <form name="myformular"> <label>Hund: </label><input type="radio" name="auswahl" value="hund" /> <label>Katze: </label><input type="radio" name="auswahl" value="katze" /> <label>Kaninchen: </label><input type="radio" name="auswahl" value="kaninchen" /> <label>Alter: </label> <input type="text" name="alter" /> <button type="button" onclick="berechnen()">Berechne</button> </form> <div class="label">Menschenalter: </div> <div id="ausgabe" class="label"></div> </body>')
+   //response.send('<body> <h2>Berechne das Menschenalter</h2> <form name="myformular"> <label>Hund: </label><input type="radio" name="auswahl" value="hund" /> <label>Katze: </label><input type="radio" name="auswahl" value="katze" /> <label>Kaninchen: </label><input type="radio" name="auswahl" value="kaninchen" /> <label>Alter: </label> <input type="text" name="alter" /> <button type="button" onclick="berechnen()">Berechne</button> </form> <div class="label">Menschenalter: </div> <div id="ausgabe" class="label"></div> </body>')
 })
 
 app.listen(app.get('port'), function() {
