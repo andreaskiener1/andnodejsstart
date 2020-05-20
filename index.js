1b5564fe66fdc2fd14dac4c56284ffa82c3e3078
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 16:23:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express')
-var path = require('path')
+//var path = require('path')
 
 var app = express()
 app.set('port', (process.env.PORT || 5000))
 
-var dateipfad = path.join(__dirname, 'public', 'caesar26.html')
+//var dateipfad = path.join(__dirname, 'public', 'caesar26.html')
  
 app.get('/', function(request, response) {
-   response.sendFile(dateipfad)
+   //response.sendFile(dateipfad)
+   response.send('Hello World!')
 })
 
 app.listen(app.get('port'), function() {
