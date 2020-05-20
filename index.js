6f8f27fa36b99433d4bea47d249991f79874b3c0
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 12:56:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express()
-
+var path = require('path')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
  
 app.get('/', function(request, response) {
   //response.send('<h1>Cäsar Verschlüsselung</h1>')
-  response.sendFile(path.join(__dirname + '/caesar26.html'))
+  response.sendFile(path.join(__dirname, 'public', 'caesar26.html'))
 })
 
 app.listen(app.get('port'), function() {
