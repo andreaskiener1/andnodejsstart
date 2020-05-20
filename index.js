4a9d5e061fbe005642284a8f083bfa97c71116d5
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 12:09:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-//app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('<h1>Hello World!</h1>')
+  response.send('Hello World!')
 })
 
 /*
