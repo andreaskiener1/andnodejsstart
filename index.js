aaca8b352def5d768da05147950d2cf97a10c6f3
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 13:52:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
 var dateipfad = path.join(__dirname, 'public', 'caesar26.html')
  
