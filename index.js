de7b3af98b65c8fa23ecc869e11ca0ed083c44bd
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 12:13:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>
+                Hello World!
+                </h1>')
 })
 
 app.listen(app.get('port'), function() {
