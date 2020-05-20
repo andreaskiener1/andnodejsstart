68afa366d92c2d05b28835389506b06967bf7f46
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 12:31:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Cäsar Verschlüsselung</h1> <form name="form1" method="post" action=""> <p>Klartext in Großbuchstaben</p> <p> <textarea rows="15" cols="50" name="eingabe"></textarea> </p> <p>Schlüssel, geben sie eine Zahl zwischen 0 und 25 ein:</p> <p> <input type="text" name="key" /> </p> <p>Verschl&uuml;sselt:</p> <p> <textarea rows="15" cols="50" name="ausgabe"></textarea> </p> <p> <input type="button" onclick="los()" value="verschlüsseln"> </p> </form>')
 })
 
 app.listen(app.get('port'), function() {
