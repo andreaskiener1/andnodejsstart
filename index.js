aaaddecc808669f92922d5ab184b4082b4a67913
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 16:56:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000))
 var dateipfad = path.join(__dirname, 'public', 'caesar26.html')
  
 app.get('/', function(request, response) {
-   //response.sendFile(dateipfad)
+    response.sendFile(dateipfad)
    //response.send('<body> <h2>Berechne das Menschenalter</h2> <form name="myformular"> <label>Hund: </label><input type="radio" name="auswahl" value="hund" /> <label>Katze: </label><input type="radio" name="auswahl" value="katze" /> <label>Kaninchen: </label><input type="radio" name="auswahl" value="kaninchen" /> <label>Alter: </label> <input type="text" name="alter" /> <button type="button" onclick="berechnen()">Berechne</button> </form> <div class="label">Menschenalter: </div> <div id="ausgabe" class="label"></div> </body>')
 })
 
