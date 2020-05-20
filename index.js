305e04ad71cf6c5b08a4c27994516ffa9cf7f6c3
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-20 16:38:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ app.set('port', (process.env.PORT || 5000))
  
 app.get('/', function(request, response) {
    //response.sendFile(dateipfad)
-   response.send('Hello World!')
+   response.send('<body>
+
+    <h2>Berechne das Menschenalter</h2>
+
+    <form name="myformular">
+        <label>Hund: </label><input type="radio" name="auswahl" value="hund" />
+        <label>Katze: </label><input type="radio" name="auswahl" value="katze" />
+        <label>Kaninchen: </label><input type="radio" name="auswahl" value="kaninchen" />
+        <label>Alter: </label>
+        <input type="text" name="alter" />
+        <button type="button" onclick="berechnen()">Berechne</button>
+    </form>
+
+    <div class="label">Menschenalter: </div>
+    <div id="ausgabe" class="label"></div>
+</body>')
 })
 
 app.listen(app.get('port'), function() {
